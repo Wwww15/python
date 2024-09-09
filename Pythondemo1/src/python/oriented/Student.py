dd1a03474177bb013696cb6b9394ae9e3ed4eb32
Administrator <admin@example.com>
2024-09-09 17:11:42 +0800

    1.定义py的包 2.partial demo

---
+++ Pythondemo1/src/python/oriented/Student.py
@@ -0,0 +1,20 @@
+class Student(object):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def print_info(self):
+        s = "name:%s,age:%d" % (self.name, self.age)
+        print(s)
+
+
+if __name__ == '__main__':
+    studentA = Student('zhangsan', 18)
+    studentB = Student('lisi', 80)
+    studentA.print_info()
+    studentB.print_info()
+
+    studentA.location = '成都高新区'
+    print(studentA.location)
+
+    

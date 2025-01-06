be41fe3ab84f16696df099c4c699b6f9f9eb5d61
Administrator <admin@example.com>
2025-01-06 18:01:39 +0800

    1.wsgi study test demo 2.flash study test demo

--- Pythondemo1/src/python/wsgioper/WSGIClientStudy.py
+++ Pythondemo1/src/python/wsgioper/WSGIClientStudy.py
@@ -1,6 +1,10 @@
 
 
 def hello(environ,start_response):
+    request_method = environ["REQUEST_METHOD"]
+    path_info = environ["PATH_INFO"]
+    print("当前请求方式:",request_method)
+    print("当前请求地址:",path_info)
     start_response('200 ok', [("Content-Type","text/html")])
     return [b'<h1>Hello,web!</h1>']
 
